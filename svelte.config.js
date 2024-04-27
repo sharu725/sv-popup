7115d971b775b9fd68dbae237d809f082b4d533d
sharathdt <sharath.dt@gmail.com>
2024-04-27 07:29:52 +0530

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
